15f5c24d29ba0db928003bbd60c253259190efe6
Dustin heaps <dustinheaps89@gmail.com>
2019-06-06 01:20:18 -0400

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { check, validationResult } = require('express-validator/check');
 const whitelist = [
   'http://localhost:3000',
   'https://still-castle-84291.herokuapp.com/',
-  'https://elated-jennings-679d2d.netlify.com/',
+  'https://clonebnb.herokuapp.com',
   'https://optimistic-spence-d73e5b.netlify.com'
 ];
 
