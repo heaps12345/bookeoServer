b823ebce01e99c836bde5514922dde4a7dd38eba
Dustin heaps <dustinheaps89@gmail.com>
2019-06-13 19:44:32 -0400

    image

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  AWS_AccessKey: 'AKIAIY66OQ5GI5TRJQOQ',
-  AWS_SecretAccessKey: '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',
-  stripeSecretKey: 'sk_test_LY3asN32gQJad6bIIsPV5afS'
+  AWS_AccessKey: 'AKIAI6SG3FEXCAZ6OWQA',
+  AWS_SecretAccessKey: '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'
+
 };
