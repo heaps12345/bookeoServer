cfce663c1a1fd53a118ec9e21a9668d636c4c3bc
Dustin heaps <dustinheaps89@gmail.com>
2019-07-10 20:01:46 -0400

    config fixes

--- server.js
+++ server.js
@@ -81,5 +81,5 @@ app.use('/reviews', reviews);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
